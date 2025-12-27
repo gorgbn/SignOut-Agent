3ae89a576cb835848b06ace4d6e236b70422cc5d
gorgbn <gorgbn@gmail.com>
2025-12-27 02:07:12 -0500

    1 commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1833924687"
+    serviceWorkerVersion: "412582819"
   }
 });
